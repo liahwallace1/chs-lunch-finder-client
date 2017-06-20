ceda371cc93edec7707a76d8409d5d3042060234
liahwallace1 <liahwallace1@gmail.com>
2017-06-20 18:26:48 -0400

    added Routes to app for Home, Filters, Restaurants, and Not Found

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+const Home = () => <div>Home</div>
+const Filters = () => <div>Filters</div>
+const Restaurants = () => <div>Restaurants</div>
+const NotFound = () => <div>Not Found</div>
 
 class App extends Component {
   constructor() {
@@ -25,18 +35,24 @@ class App extends Component {
     console.log(fetchingData)
     console.log(restaurantData)
     return (
-      <div className="App">
-        <div className="App-header">
+      <Router>
+        <div className="App">
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/filters" component={Filters}/>
+        <Route exact path="/restaurants" component={Restaurants}/>
+        <Route component={NotFound} />
+          <div className="App-header">
 
-          <h2>What's' for Lunch Charleston</h2>
+            <h2>What's' for Lunch Charleston</h2>
+          </div>
+          <div className="App-intro">
+            {fetchingData ?
+              <img src={logo} className="App-logo" alt="logo" />
+              :
+              <h1>Have data</h1>}
+          </div>
         </div>
-        <div className="App-intro">
-          {fetchingData ?
-            <img src={logo} className="App-logo" alt="logo" />
-            :
-            <h1>Have data</h1>}
-        </div>
-      </div>
+      </Router>
     );
   }
 }
