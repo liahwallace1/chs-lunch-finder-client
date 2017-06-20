781e549db2f92004415c47820002d8c61b8804e1
liahwallace1 <liahwallace1@gmail.com>
2017-06-20 16:52:21 -0400

    create initial App.js state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      fetchingData: true,
+      restaurantData: []
+    }
+  }
+
   render() {
+    const { fetchingData } = this.state
+
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
+          <h2>What's' for Lunch Charleston</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {fetchingData ?
+            <img src={logo} className="App-logo" alt="logo" />
+            :
+            <h1>Have data</h1>}
         </p>
       </div>
     );
