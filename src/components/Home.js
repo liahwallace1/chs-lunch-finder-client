9bed1c2ff006cd5a4b69fcfe9ee759fb5123e846
liahwallace1 <liahwallace1@gmail.com>
2017-06-21 10:36:14 -0400

    updated Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import 'semantic-ui-css/semantic.min.css';
 
 const Home = () =>
   <div>
-    <h4>Can't decide where to go to lunch in Charleston? Let us help you out! See a list of restaurants, or let us choose for you!</h4>
+    <h4>Can't' decide where to go to lunch in Charleston? Let us help you out! See a list of restaurants, or let us choose for you!</h4>
     <br />
     <br />
     <Link to="/restaurants"><Button inverted color='purple'>Restaurant List</Button></Link>
