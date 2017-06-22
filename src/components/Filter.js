8ca39677dfb51035f99a6b1beff90ee207a0633e
liahwallace1 <liahwallace1@gmail.com>
2017-06-22 15:00:45 -0400

    commented out features for now - come back later

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import './Filter.css'
 // import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react';
 // import 'semantic-ui-css/semantic.min.css';
