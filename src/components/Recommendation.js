0be392b18d23938e84e02ecc39f5b0f184258b45
liahwallace1 <liahwallace1@gmail.com>
2017-06-22 16:08:41 -0400

    added information to Recommendation page, added css file for recommendation page

--- src/components/Recommendation.js
+++ src/components/Recommendation.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Icon } from 'semantic-ui-react'
+import './Recommendation.css'
 
 
 export class Recommendation extends Component {
   render() {
   let funny_intros = ["You know the lunch room is full of weirdos. Take a break at ", "Microwave lines killing your day? Head over to "]
   let restaurants = this.props.restaurantData
-  let recommended_restaurant = restaurants[Math.floor(Math.random()*restaurants.length)];
+  let rec_restaurant = restaurants[Math.floor(Math.random()*restaurants.length)];
   let intro = funny_intros[Math.floor(Math.random()*funny_intros.length)];
-  console.log(recommended_restaurant)
+
+  console.log(rec_restaurant)
+
     return (
     <div>
-      <h1>{intro}<span className="recommended-restaurant">{recommended_restaurant.name}</span>!</h1>
+      <div>
+        <h1>{intro}<a href={rec_restaurant.yelp_url} className="recommended-restaurant">{rec_restaurant.name}</a>!</h1>
+      </div>
 
+      <div className="restaurant-info">
+
+        <div className="restaurant-type">
+          <div><Icon circular inverted color='teal' name='spoon' /></div>
+          <div><p>{rec_restaurant.category_list}</p></div>
+        </div>
+
+        <div className="restaurant-price">
+          <p>{rec_restaurant.price}</p>
+        </div>
+
+        <div className="restaurant-takeout">
+          <div>{rec_restaurant.takeout ? <Icon circular inverted color='teal' name='thumbs up' /> : <Icon circular inverted color='orange' name='thumbs down' />}</div>
+          <div><p>Takeout</p></div>
+        </div>
+
+        <div className="restaurant-delivery">
+          <div>{rec_restaurant.delivery ? <Icon circular inverted color='teal' name='thumbs up' /> : <Icon circular inverted color='orange' name='thumbs down' />}</div>
+          <div><p>Delivery</p></div>
+        </div>
+
+      </div>
     </div>
     )
   }
--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -3,10 +3,10 @@ import { Image, Segment } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
 const Restaurant = (restaurant) => {
- const categories = []
- restaurant.restaurant.categories.map((category) => {
-   return categories.push(category.title)
- })
+ // const categories = []
+ // restaurant.restaurant.categories.map((category) => {
+ //   return categories.push(category.title)
+ // })
 
   return (
     <div key={restaurant.restaurant.id} className="restaurant">
@@ -14,7 +14,7 @@ const Restaurant = (restaurant) => {
           <a href={restaurant.restaurant.yelp_url}><Image src={restaurant.restaurant.image_url} size='small' floated='left' /></a>
           <h3>{restaurant.restaurant.name}</h3>
           <p>{restaurant.restaurant.price}     -     Rating: {restaurant.restaurant.yelp_rating}</p>
-          <p>Type: {categories.join(", ")}</p>
+          <p>Type: {restaurant.restaurant.category_list}</p>
           <p>Takout? {restaurant.restaurant.takeout ? "Yes" : "Nope"} -  Delivery? {restaurant.restaurant.delivery ? "Yes" : "Nope"}</p>
           <p>{restaurant.restaurant.address}, {restaurant.restaurant.city}, {restaurant.restaurant.state} {restaurant.restaurant.zip_code} - {restaurant.restaurant.display_phone}</p>
       </Segment>
