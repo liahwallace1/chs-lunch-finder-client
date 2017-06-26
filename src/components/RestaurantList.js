8bac4db87d09f5d9b335f6c865f11a87302f334e
liahwallace1 <liahwallace1@gmail.com>
2017-06-26 14:55:04 -0400

    imported Semantic UI Item feature

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,38 +1,18 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
+import React from 'react';
 import { Item } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 import Restaurant from '../components/Restaurant';
-import Filter from '../components/Filter';
 
-// import { toggleFilter } from '../actions/filterActions';
+const RestaurantList = ({ restaurants }) => {
 
-export class Restaurants extends Component {
-  render() {
-    const restaurants = this.props.restaurantData.map((restaurant, index) => {
-      return <div key={restaurant.id}><Restaurant restaurant={restaurant} /></div>
-    })
-    return (
-      <div>
-        <h3>Restaurants in Charleston, SC</h3>
-        <Filter  />
-        <Item.Group divided unstackable>
-          {restaurants}
-        </Item.Group>
-      </div>
-    )
-  }
+  return (
+    <Item.Group divided unstackable>
+      {restaurants.map(restaurant => (
+        <Restaurant key={restaurant.id} restaurant={restaurant} />
+      ))}
+    </Item.Group>
+
+  )
 }
 
-const mapStateToProps = (state) => {
-  return {
-    restaurantData: state.restaurantDataReducer,
-    // toggleFilter: state.filterReducer
-  }
-}
-
-// const mapDispatchToProps = (dispatch) => {
-//   return bindActionCreators({ toggleFilter: toggleFilter }, dispatch)
-// }
-
-export const ConnectedRestaurants = connect(mapStateToProps)(Restaurants)
+export default RestaurantList
