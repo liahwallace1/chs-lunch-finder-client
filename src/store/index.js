a49784646e2c229f7ad2ed164748215198b13f12
liahwallace1 <liahwallace1@gmail.com>
2017-06-26 12:12:14 -0400

    working on filter reducer

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,6 @@ import {
   applyMiddleware,
   combineReducers
 } from 'redux';
-import { reducert as filterReducer, filterActions } from 'redux-filter';
 import thunk from 'redux-thunk';
 import restaurantDataReducer from '../reducers/restaurantDataReducer';
 import fetchingDataReducer from '../reducers/fetchingDataReducer';
@@ -12,33 +11,12 @@ import filterReducer from '../reducers/filterReducer';
 const reducers = combineReducers({
   fetchingDataReducer,
   restaurantDataReducer,
-  filterReducer,
+  visibilityFilterReducer,
 })
 const middleware = [thunk]
 
 export default createStore(
   reducers,
-  filters: {
-    restaurants: window.RESTAURANTS,
-    filterableCriteria: [
-      {
-        title: 'price',
-        attribute: 'price'
-    },
-      {
-        title: 'location',
-        attribute: 'zip_code'
-      },
-      {
-        title: 'features',
-        attribute: 'takeout',
-        attribute: 'delivery'
-      }
-    ],
-    searchKeys: ['title'],
-  }
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(...middleware),
 )
-
-store.dispatch(filterActions.init());
