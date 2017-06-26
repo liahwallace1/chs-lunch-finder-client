e38c61483a78577687cac9e5fdf15685435d8a8d
liahwallace1 <liahwallace1@gmail.com>
2017-06-26 14:56:50 -0400

    updated visibility filter to just have actions to add and remove filters from price and location, will use to get restaurantsFiltered

--- src/reducers/visibilityFilterReducer.js
+++ src/reducers/visibilityFilterReducer.js
@@ -1,24 +1,24 @@
 let initialState = {
-  visible: true,
-  priceFilter: ['$', '$$', '$$$', '$$$$'],
-  locationFilter: ['29401', '29403'],
-  takeoutFilter: false,
-  deliveryFilter: false,
+  restaurantFilters: [
+    {priceFilter: []},
+    {locationFilter: []},
+  ]
 }
 
 const visibilityFilter = (state = initialState, action) => {
   switch (action.type) {
     // case 'TOGGLE_FILTER':
     //   return {visible: state.visible.toggle}
-    case 'FILTER_PRICE':
-      const checkedPrices = []
-      return {priceFilter: restaurantData.filter((restaurant) => restaurant.price == action.price)}
-    case 'FILTER_LOCATION':
-      return {locationFilter: restaurantData.filter((restaurant) => restaurant.zip_code == action.value)}
-    case 'FILTER_TAKEOUT':
-      return {takoutFilter: restaurantData.filter((restaurant) => restaurant.takeout)}
-      case 'FILTER_DELIVERY':
-        return {deliveryFilter: restaurantData.filter((restaurant) => restaurant.delivery)}
+    case 'ADD_PRICE_FILTER':
+      return state.priceFilter.push(action.payload)
+    case 'REMOVE_PRICE_FILTER':
+      let price_index = state.priceFilter.indexOf(action.payload)
+      return state.priceFilter.splice(price_index, 1)
+    case 'ADD_LOCATION_FILTER':
+      return state.locationFilter.push(action.payload)
+      case 'REMOVE_LOCATION_FILTER':
+      let location_index = state.locationFilter.indexOf(action.payload)
+      return state.locationFilter.splice(location_index, 1)
     default:
       return state
   }
