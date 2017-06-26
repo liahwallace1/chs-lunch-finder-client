16bef64127cffaa2ee3cd220a618330580e83aac
liahwallace1 <liahwallace1@gmail.com>
2017-06-26 18:34:29 -0400

    got add price and add location reducers to update state corectly

--- src/reducers/visibilityFilterReducer.js
+++ src/reducers/visibilityFilterReducer.js
@@ -1,24 +1,24 @@
 let initialState = {
-  restaurantFilters: [
-    {priceFilter: []},
-    {locationFilter: []},
-  ]
-}
+    priceFilter: [],
+    locationFilter: [],
+  }
 
 const visibilityFilter = (state = initialState, action) => {
   switch (action.type) {
     // case 'TOGGLE_FILTER':
     //   return {visible: state.visible.toggle}
     case 'ADD_PRICE_FILTER':
-      return state.restaurantFilters[0].priceFilter.push(action.payload)
-    case 'REMOVE_PRICE_FILTER':
-      let price_index = state.restaurantFilters[0].priceFilter.indexOf(action.payload)
-      return state.restaurantFilters[0].priceFilter.splice(price_index, 1)
+      return Object.assign({}, state, { priceFilter: [...state.priceFilter, action.priceType], locationFilter: [...state.locationFilter]})
+
+    // case 'REMOVE_PRICE_FILTER':
+    //   debugger
+    //   let price_index = state.restaurantFilters[0].priceFilter.indexOf(action.priceType)
+    //   return state.restaurantFilters[0].priceFilter.splice(price_index, 1)
     case 'ADD_LOCATION_FILTER':
-      return state.restaurantFilters[1].locationFilter.push(action.payload)
-      case 'REMOVE_LOCATION_FILTER':
-      let location_index = state.restaurantFilters[1].locationFilter.indexOf(action.payload)
-      return state.restaurantFilters[1].locationFilter.splice(location_index, 1)
+      return Object.assign({}, state, { priceFilter: [...state.priceFilter], locationFilter: [...state.locationFilter, action.zip_code]})
+    //   case 'REMOVE_LOCATION_FILTER':
+    //   let location_index = state.restaurantFilters[1].locationFilter.indexOf(action.zip_code)
+    //   return state.restaurantFilters[1].locationFilter.splice(location_index, 1)
     default:
       return state
   }
