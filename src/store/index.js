00818489b2010acd1395d953fdc66afba76c1a82
liahwallace1 <liahwallace1@gmail.com>
2017-06-26 10:25:20 -0400

    configuring redux-filter to filter restaurants

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import {
   applyMiddleware,
   combineReducers
 } from 'redux';
+import { reducert as filterReducer, filterActions } from 'redux-filter';
 import thunk from 'redux-thunk';
 import restaurantDataReducer from '../reducers/restaurantDataReducer';
 import fetchingDataReducer from '../reducers/fetchingDataReducer';
@@ -17,6 +18,27 @@ const middleware = [thunk]
 
 export default createStore(
   reducers,
+  filters: {
+    restaurants: window.RESTAURANTS,
+    filterableCriteria: [
+      {
+        title: 'price',
+        attribute: 'price'
+    },
+      {
+        title: 'location',
+        attribute: 'zip_code'
+      },
+      {
+        title: 'features',
+        attribute: 'takeout',
+        attribute: 'delivery'
+      }
+    ],
+    searchKeys: ['title'],
+  }
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(...middleware),
 )
+
+store.dispatch(filterActions.init());
