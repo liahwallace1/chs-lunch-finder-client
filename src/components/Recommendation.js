e3d184a4981f3e495f63b78a2e1c82f8d929576f
Liahwallace1 <Liahwallace1@gmail.com>
2017-06-29 16:55:58 -0400

    added buttons to Recommendation page, edited css

--- src/components/Recommendation.js
+++ src/components/Recommendation.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Grid, Icon } from 'semantic-ui-react'
+import { Grid, Icon, Button } from 'semantic-ui-react'
 import './Recommendation.css'
 
 
@@ -19,31 +19,35 @@ class Recommendation extends Component {
           <h1>{intro}<a href={rec_restaurant.yelp_url} className="recommended-restaurant">{rec_restaurant.name}</a>!</h1>
         </div>
       <br />
-      <Grid className="restaurant-info" columns={2} divided>
-        <Grid.Row>
-          <Grid.Column className="restaurant-type">
-            <div><Icon circular inverted color='teal' name='food' /></div>
-            <div><p>{rec_restaurant.category_list}</p></div>
-          </Grid.Column>
+        <Grid className="restaurant-info" columns={2} stackable verticalAlign='middle'>
+          <Grid.Row>
+            <Grid.Column className="restaurant-type">
+              <div><Icon circular inverted size='large' color='teal' name='food' /></div>
+              <div><p>{rec_restaurant.category_list}</p></div>
+            </Grid.Column>
 
-          <Grid.Column className="restaurant-price">
-            <div><Icon circular inverted color='olive' name='currency' /></div>
-            <p>{rec_restaurant.price}</p>
-          </Grid.Column>
-        </Grid.Row>
+            <Grid.Column className="restaurant-price">
+              <div><Icon circular inverted size='large' color='olive' name='currency' /></div>
+              <p>{rec_restaurant.price}</p>
+            </Grid.Column>
+          </Grid.Row>
 
-        <Grid.Row>
-          <Grid.Column className="restaurant-takeout">
-            <div>{rec_restaurant.takeout ? <Icon circular inverted color='teal' name='thumbs up' /> : <Icon circular inverted color='orange' name='thumbs down' />}</div>
-            <div><p>Takeout</p></div>
-          </Grid.Column>
+          <Grid.Row>
+            <Grid.Column className="restaurant-takeout">
+              <div>{rec_restaurant.takeout ? <Icon circular inverted size='large' color='purple' name='thumbs up' /> : <Icon circular inverted size='large' color='orange' name='thumbs down' />}</div>
+              <div><p>Takeout</p></div>
+            </Grid.Column>
 
-          <Grid.Column className="restaurant-delivery">
-            <div>{rec_restaurant.delivery ? <Icon circular inverted color='teal' name='thumbs up' /> : <Icon circular inverted color='orange' name='thumbs down' />}</div>
-            <div><p>Delivery</p></div>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+            <Grid.Column className="restaurant-delivery">
+              <div>{rec_restaurant.delivery ? <Icon circular inverted size='large' color='purple' name='thumbs up' /> : <Icon circular inverted size='large' color='orange' name='thumbs down' />}</div>
+              <div><p>Delivery</p></div>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <div className="recom-buttons">
+          <Button className="recom-button" size="large" color="red"><Icon name='refresh' />New Suggestion</Button>
+          <Button className="recom-button" size="large" color="blue" padding="10px"><Icon name='map pin' />Where is it?</Button>
+        </div>
       </div>
     </div>
     )
