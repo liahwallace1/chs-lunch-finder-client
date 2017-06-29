0c56085ac90799aa567f9be5b14799ce18071e22
Liahwallace1 <Liahwallace1@gmail.com>
2017-06-29 19:37:42 -0400

    added style and map component to Google Api container

--- src/containers/GoogleMapApiComponent.js
+++ src/containers/GoogleMapApiComponent.js
@@ -15,9 +15,22 @@ GoogleApi({
 
 const Container = React.createClass({
   render: function() {
-    return <div>Google</div>;
+    const style = {
+      width: '100vw',
+      height: '100vh'
+
+    if (!this.props.loaded) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div style={style}>
+        <Map google={this.props.google} />
+      </div>
+    )
   }
 })
+
+
 export default GoogleApiComponent({
   apiKey: __GAPI_KEY__
 })(Container)
