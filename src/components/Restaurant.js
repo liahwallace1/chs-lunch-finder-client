b3477aae0143c85008333ef2b953894d0cf9f101
Liahwallace1 <Liahwallace1@gmail.com>
2017-06-29 14:32:19 -0400

    updated formatting of restaurant list

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -6,25 +6,23 @@ import './Restaurant.css'
 const Restaurant = ({ restaurant }) => {
 
   return (
-    <Segment key={restaurant.id} className="restaurant" clearing>
-      <Item>
-        <Item.Image src={restaurant.image_url} size='small' floated="left"	/>
+    <Item key={restaurant.id} className="restaurant" clearing>
+      <Item.Image src={restaurant.image_url} size='small'	/>
 
-        <Item.Content>
-          <Item.Header as='a'>{restaurant.name}</Item.Header>
-            <Item.Extra>
-              <Label>Cost: {restaurant.price}</Label>
-              <Label>Rating: {restaurant.yelp_rating}</Label>
-              <Label>Takout? {restaurant.takeout ? <Icon name='thumbs up' /> : <Icon name='thumbs down' />}</Label>
-              <Label>Delivery? {restaurant.delivery ? <Icon name='thumbs up' /> : <Icon name='thumbs down' />}</Label>
-            </Item.Extra>
-            <p><strong>Type:</strong> {restaurant.category_list}</p>
-            <Item.Description>
-              {restaurant.address}, {restaurant.city}, {restaurant.state} {restaurant.zip_code} - {restaurant.display_phone}
-            </Item.Description>
-        </Item.Content>
-      </Item>
-    </Segment>
+      <Item.Content verticalAlign='middle'>
+        <Item.Header as='a'>{restaurant.name}</Item.Header>
+          <Item.Extra>
+            <Label>Cost: {restaurant.price}</Label>
+            <Label>Rating: {restaurant.yelp_rating}</Label>
+            <Label>Takout? {restaurant.takeout ? <Icon name='thumbs up' /> : <Icon name='thumbs down' />}</Label>
+            <Label>Delivery? {restaurant.delivery ? <Icon name='thumbs up' /> : <Icon name='thumbs down' />}</Label>
+          </Item.Extra>
+          <p><strong>Type:</strong> {restaurant.category_list}</p>
+          <Item.Description>
+            {restaurant.address}, {restaurant.city}, {restaurant.state} {restaurant.zip_code} - {restaurant.display_phone}
+          </Item.Description>
+      </Item.Content>
+    </Item>
 
   )
 }
