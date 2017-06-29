6e30de7ee95014d8eef16f5553ade847a6345945
Liahwallace1 <Liahwallace1@gmail.com>
2017-06-29 10:35:58 -0400

    added reducer actions to add and remove takeout and delivery filters

--- src/reducers/visibilityFilterReducer.js
+++ src/reducers/visibilityFilterReducer.js
@@ -1,6 +1,8 @@
 let initialState = {
     priceFilter: [],
     locationFilter: [],
+    takeoutFilter: false,
+    deliveryFilter: false,
   }
 
 const visibilityFilter = (state = initialState, action) => {
@@ -8,18 +10,30 @@ const visibilityFilter = (state = initialState, action) => {
     // case 'TOGGLE_FILTER':
     //   return {visible: state.visible.toggle}
     case 'ADD_PRICE_FILTER':
-      return Object.assign({}, state, { priceFilter: [...state.priceFilter, action.priceType], locationFilter: [...state.locationFilter]})
+      return Object.assign({}, state, { priceFilter: [...state.priceFilter, action.priceType], locationFilter: [...state.locationFilter], takeoutFilter: state.takeoutFilter, deliveryFilter: state.deliveryFilter})
 
     case 'REMOVE_PRICE_FILTER':
       const newPriceFilter = state.priceFilter.filter((item) => item !== action.priceType)
-      return { priceFilter: newPriceFilter, locationFilter: [...state.locationFilter]}
+      return { priceFilter: newPriceFilter, locationFilter: [...state.locationFilter], takeoutFilter: state.takeoutFilter, deliveryFilter: state.deliveryFilter}
 
       case 'ADD_LOCATION_FILTER':
-      return Object.assign({}, state, { priceFilter: [...state.priceFilter], locationFilter: [...state.locationFilter, action.zip_code]})
+      return Object.assign({}, state, { priceFilter: [...state.priceFilter], locationFilter: [...state.locationFilter, action.zip_code], takeoutFilter: state.takeoutFilter, deliveryFilter: state.deliveryFilter})
 
     case 'REMOVE_LOCATION_FILTER':
       const newLocationFilter = state.locationFilter.filter((item) => item !== action.zip_code)
-      return { priceFilter: [...state.priceFilter], locationFilter: newLocationFilter}
+      return { priceFilter: [...state.priceFilter], locationFilter: newLocationFilter, takeoutFilter: state.takeoutFilter, deliveryFilter: state.deliveryFilter}
+
+    case 'ADD_TAKEOUT_FILTER':
+      return { priceFilter: [...state.priceFilter], locationFilter: [...state.locationFilter], takeoutFilter: true, deliveryFilter: state.deliveryFilter}
+
+    case 'REMOVE_TAKEOUT_FILTER':
+      return { priceFilter: [...state.priceFilter], locationFilter: [...state.locationFilter], takeoutFilter: false, deliveryFilter: state.deliveryFilter}
+
+    case 'ADD_DELIVERY_FILTER':
+      return { priceFilter: [...state.priceFilter], locationFilter: [...state.locationFilter], takeoutFilter: state.takeoutFilter, deliveryFilter: true}
+
+    case 'REMOVE_DELIVERY_FILTER':
+      return { priceFilter: [...state.priceFilter], locationFilter: [...state.locationFilter], takeoutFilter: state.takeoutFilter, deliveryFilter: false}
 
     default:
       return state
