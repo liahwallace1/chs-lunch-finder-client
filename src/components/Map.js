9a8123a3b0f145c2c9946217835c51844bf66d5b
Liahwallace1 <Liahwallace1@gmail.com>
2017-06-29 19:40:02 -0400

    added Google Map component

---
+++ src/components/Map.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export class Map extends React.Component {
+  render() {
+    return (
+      <div ref='map'>
+        Loading map...
+      </div>
+    )
+  }
+}
