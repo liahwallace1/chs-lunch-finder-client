a16255a3e773549aeaee646ccfa25ee46ef74f21
Liahwallace1 <Liahwallace1@gmail.com>
2017-06-30 17:17:49 -0400

    updated variables and GoogleApi version

--- src/containers/GoogleMapWrapper.js
+++ src/containers/GoogleMapWrapper.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import Map, {GoogleApiWrapper} from 'google-maps-react';
 import { Header, Sidebar, Segment } from 'semantic-ui-react';
@@ -9,11 +8,11 @@ class GoogleMapWrapper extends Component {
   onMapMove() {}
 
   render() {
-    const restaurant = this.props.toggleMap.restaurant;
+    const restaurant = this.props.recommendation.restaurant;
 
     return (
       <Sidebar.Pushable as={Segment}>
-        <Sidebar animation='overlay' direction='bottom' visible={this.props.toggleMap.visible} >
+        <Sidebar animation='overlay' direction='bottom' visible={this.props.recommendation.map_visible} >
           <Header size='medium'>
             {restaurant.address}<br />
             {restaurant.city}, {restaurant.state} {restaurant.zip_code}
@@ -30,10 +29,10 @@ class GoogleMapWrapper extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    toggleMap: state.toggleMap,
+    recommendation: state.recommendation,
   }
 }
 
 export const ConnectedGoogleMapWrapper = connect(mapStateToProps)(GoogleApiWrapper({
-  apiKey: 'AIzaSyBHctnD2biEXNByZnGKdXRq836H0WPHu-8'
+  apiKey: 'AIzaSyBHctnD2biEXNByZnGKdXRq836H0WPHu-8', version: '3.27'
 })(GoogleMapWrapper))
