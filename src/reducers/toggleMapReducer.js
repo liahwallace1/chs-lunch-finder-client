1f63e77a3e2a26ab6aaf7469f49975d8863296f8
Liahwallace1 <Liahwallace1@gmail.com>
2017-06-30 15:11:25 -0400

    added restaurant to state and updated action

--- src/reducers/toggleMapReducer.js
+++ src/reducers/toggleMapReducer.js
@@ -1,9 +1,17 @@
-const toggleMap = (state = false, action) => {
+let initialState = {
+    visible: false,
+    restaurant: {}
+  }
+
+const toggleMap = (state = initialState, action) => {
   switch (action.type) {
     case 'TOGGLE_MAP':
-      return !state.toggleMap
+      const newRestaurant = action.rec_restaurant
+      return {visible: !state.visible, restaurant: newRestaurant}
 
     default:
       return state
   }
 }
+
+export default toggleMap
