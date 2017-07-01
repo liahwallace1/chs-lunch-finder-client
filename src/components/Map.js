e3f2d167def6080fdc8aede9ad6c58eecdbed810
Liahwallace1 <Liahwallace1@gmail.com>
2017-06-30 20:47:00 -0400

    still trying to get map to work

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,43 +1,70 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { connect } from 'react-redux';
-import Map, {GoogleApiWrapper, Marker} from 'google-maps-react';
-import PropTypes from 'prop-types';
-
-
+// import { connect } from 'react-redux';
+import {GoogleApiWrapper, Marker} from 'google-maps-react';
+// import PropTypes from 'prop-types';
 
 class Map extends Component {
-  _renderMarkers() {
-   let r = this.props.toggleMap.restaurant
-   let pos = {lat: r.latitude, lng: r.longitude}
-   return <Marker
-               key={r.id}
-               label={r.name}
-               map={this.props.map}
-               position={pos} />
-   });
- }
+
+
+  loadMap() {
+    debugger
+    if (this.props && this.props.google) {
+      // google is available
+      const {google} = this.props;
+      const maps = google.maps;
+
+      const mapRef = this.refs.map;
+      const node = ReactDOM.findDOMNode(mapRef);
+      console.log(node)
+
+      let zoom = 14;
+      let lat = 32.532543;
+      let lng = -79.342354;
+      const center = new maps.LatLng(lat, lng);
+      const mapConfig = Object.assign({}, {
+        center: center,
+        zoom: zoom
+      })
+      this.map = new maps.Map(node, mapConfig);
+    }
+  }
 
  render() {
-
+   this.loadMap()
    return (
-     <Map map={this.props.map}
-       google={this.props.google}
-       className={styles.map}
-       zoom={this.props.zoom}
-       onRecenter={this.props.onMove}
-       onDragend={this.props.onMove}
-       visible={!children || React.Children.count(children) == 0}
-       >
-     </Map>
+     <div ref='map'>
+      Loading Map
+    </div>
    )
  }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    toggleMap: state.toggleMap,
-  }
-}
 
-export const ConnectedMap = connect(mapStateToProps)(Map)
+// const mapStateToProps = (state) => {
+//   return {
+//     recommendation: state.recommendation,
+//   }
+// }
+
+export default Map
+
+// export const ConnectedMap = connect(mapStateToProps)(Map)
+
+//  _renderMarkers() {
+//   let r = this.props.recommendation.restaurant
+//   let pos = {lat: r.latitude, lng: r.longitude}
+//   return <Marker
+//               key={r.id}
+//               label={r.name}
+//               map={this.props.map}
+//               position={pos} />
+//   });
+// }
+
+// <Map map={this.props.map}
+//   google={this.props.google}
+//   zoom={this.props.zoom}
+//   onRecenter={this.props.onMove}
+//   onDragend={this.props.onMove}
+//   />
