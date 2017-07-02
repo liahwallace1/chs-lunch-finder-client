a8b6446591efff97044fb1e62ce551d67121609b
Liahwallace1 <Liahwallace1@gmail.com>
2017-07-02 12:44:17 -0400

    added state and reducer action for map width

--- src/reducers/recommendationReducer.js
+++ src/reducers/recommendationReducer.js
@@ -1,5 +1,6 @@
 let initialState = {
     map_visible: false,
+    map_width: 0,
     restaurant: {},
     funny_intros: ["You know the lunch room is full of weirdos. Take a break at ", "A line at the microwave killing your day? Head over to ", "Indecisive much? You should go to "],
     current_intro: "",
@@ -9,9 +10,11 @@ const recommendation = (state = initialState, action) => {
   switch (action.type) {
     case 'SET_RECOMMENDATION':
       const newIntro = state.funny_intros[Math.floor(Math.random()*state.funny_intros.length)]
-      return {map_visible: state.map_visible, restaurant: action.restaurant, funny_intros: [...state.funny_intros], current_intro: newIntro}
+      return {map_visible: state.map_visible, map_width: state.map_width, restaurant: action.restaurant, funny_intros: [...state.funny_intros], current_intro: newIntro}
     case 'TOGGLE_MAP':
-      return {map_visible: !state.map_visible, restaurant: state.restaurant, funny_intros: [...state.funny_intros], current_intro: state.current_intro}
+      return {map_visible: !state.map_visible, map_width: window.innerWidth - 90, restaurant: state.restaurant, funny_intros: [...state.funny_intros], current_intro: state.current_intro}
+      case 'UPDATE_MAP_WIDTH':
+        return {map_visible: state.map_visible, map_width: window.innerWidth - 90, restaurant: state.restaurant, funny_intros: [...state.funny_intros], current_intro: state.current_intro}
 
     default:
       return state
