324caf6728d595ef3ef29fdae5d1340637e29575
liahwallace1 <liahwallace1@gmail.com>
2017-07-09 12:39:34 -0400

    updated state to include restaurant id

--- src/reducers/hashtagReducer.js
+++ src/reducers/hashtagReducer.js
@@ -1,14 +1,14 @@
 let initialState = {
     hashtagOptions: [],
-    selectedHashtags: []
+    selectedHashtags: {restaurantId: null, hashtags: []}
   }
 
 const hashtag = (state = initialState, action) => {
   switch (action.type) {
     case 'RECEIVE_HASHTAG_OPTIONS':
-      return Object.assign({}, state, { hashtagOptions: action.hashtagOptions, selectedHashtags: [...state.selectedHashtags]})
+      return Object.assign({}, state, { hashtagOptions: action.hashtagOptions, selectedHashtags: {...state.selectedHashtags}})
     case 'ADD_SELECTED_HASHTAG':
-      return Object.assign({}, state, { hashtagOptions: [state.hashtagOptions], selectedHashtags: action.selectedHashtags})
+      return Object.assign({}, state, { hashtagOptions: state.hashtagOptions, selectedHashtags: {restaurantId: action.restaurantId, hashtags: action.selectedHashtags}})
     default:
       return state
   }
