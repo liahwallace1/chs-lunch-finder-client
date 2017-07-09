ebcf0a9af8da37640ccb54985d8add926971515f
liahwallace1 <liahwallace1@gmail.com>
2017-07-09 11:39:04 -0400

    added image and features to modal

--- src/components/AddHashtagForm.js
+++ src/components/AddHashtagForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Icon, Button, Dropdown, Form } from 'semantic-ui-react';
+import { Icon, Button, Dropdown, Form, Modal, Image, Header } from 'semantic-ui-react';
 
 const getHashtagOptions = (hashtags) => {
   var options = []
@@ -13,12 +13,21 @@ class AddHashtagForm extends Component {
   render() {
     var hashtagOptions = getHashtagOptions(this.props.hashtag)
     return (
-      <Form>
-        <Form.Group inline>
-          <Dropdown placeholder='Add Hashtag' floating button className='icon' multiple search selection options={hashtagOptions} />
-          <Button icon="checkmark" onClick={(e) => this.manageHashtag(e)} />
-        </Form.Group>
-      </Form>
+      <Modal trigger={<Button small>Add Hashtag</Button>} closeIcon='close'>
+        <Modal.Header>Add a Hashtag:</Modal.Header>
+        <Modal.Content>
+         <Image wrapped size='small' src={this.props.restaurant.image_url} floated='left' />
+         <Modal.Description>
+           <Header>{this.props.restaurant.name}</Header>
+            <Form>
+              <Form.Group inline>
+                <Dropdown placeholder='Add Hashtag' floating button className='icon' multiple search selection options={hashtagOptions} />
+                <Button icon="checkmark" onClick={(e) => this.manageHashtag(e)} />
+              </Form.Group>
+            </Form>
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
     )
   }
 }
