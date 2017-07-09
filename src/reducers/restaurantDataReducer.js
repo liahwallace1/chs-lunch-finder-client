d6a6aaf5bc65c10c3da8604859c75da00b26adbf
liahwallace1 <liahwallace1@gmail.com>
2017-07-09 16:33:10 -0400

    added reducer to update restaurantData after posting new hashtags

--- src/reducers/restaurantDataReducer.js
+++ src/reducers/restaurantDataReducer.js
@@ -2,6 +2,11 @@ const restaurantData = (state = [], action) => {
   switch (action.type) {
     case 'RECEIVE_RESTAURANT_DATA':
       return action.restaurantData;
+    case 'UPDATE_RESTAURANT_DATA':
+      return [
+        ...state.filter(restaurant => restaurant.id !== action.restaurant.id),
+        Object.assign({}, action.restaurant)
+      ]
     default:
       return state
   }
