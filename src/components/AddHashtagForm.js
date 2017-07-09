0fd363b98805fa4ab5a985ffa49c26b1c54cec99
liahwallace1 <liahwallace1@gmail.com>
2017-07-09 16:29:45 -0400

    figuring out form submit actions

--- src/components/AddHashtagForm.js
+++ src/components/AddHashtagForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button, Dropdown, Form, Modal, Image, Header } from 'semantic-ui-react';
-import { addSelectedHashtag } from '../actions/hashtagActions';
+import { addSelectedHashtag, sendRestaurantHashtags } from '../actions/hashtagActions';
+import { updateRestaurantState } from '../actions/restaurantDataActions';
 
 const getHashtagOptions = (hashtags) => {
   var options = []
@@ -15,21 +16,28 @@ class AddHashtagForm extends Component {
     this.props.dispatch(addSelectedHashtag(this.props.restaurant.id, data.value))
   }
 
-  manageHashtag(e, data) {
+  handleSubmit(e) {
     e.preventDefault();
-    console.log(data.value)
+    var data = this.props.hashtag.selectedHashtags
+    var restaurant = this.props.restaurantData.filter(r => r.id === data.restaurantId)[0]
+    var combinedHashtags = restaurant.hashtags_list.concat(data.hashtags).filter((x, i, a) => a.indexOf(x) === i)
+    var updatedRestaurant = Object.assign({}, restaurant, {
+      hashtags_list: combinedHashtags})
+      console.log(updatedRestaurant)
+    // this.props.dispatch(updateRestaurant(updatedRestaurant))
   }
 
   render() {
     var hashtagOptions = getHashtagOptions(this.props.hashtag.hashtagOptions)
     return (
-      <Modal trigger={<Button size='small'>Add Hashtag</Button>} closeIcon='close'>
+      <Modal trigger={<Button size='small'>Add Hashtag</Button>} closeIcon='close'
+      >
         <Modal.Header>Add a Hashtag:</Modal.Header>
         <Modal.Content>
          <Image wrapped size='small' src={this.props.restaurant.image_url} floated='left' />
          <Modal.Description>
            <Header>{this.props.restaurant.name}</Header>
-            <Form onSubmit={(e) => this.manageHashtag(e)}>
+            <Form onSubmit={(e) => this.handleSubmit(e)}>
               <Form.Group inline>
                 <Dropdown placeholder='Add Hashtag' floating button className='icon' multiple search selection options={hashtagOptions} onChange={this.setValue.bind(this)}/>
                 <Button icon="checkmark" type='submit' />
@@ -37,6 +45,7 @@ class AddHashtagForm extends Component {
             </Form>
           </Modal.Description>
         </Modal.Content>
+
       </Modal>
     )
   }
